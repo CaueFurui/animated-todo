b9a12a0781950fab09c84e8acf3c8c8e0b3c7583
CaueFurui <cauefurui@gmail.com>
2021-11-22 15:05:54 -0300

    feat(tasklabel): adding task label animatated

---
+++ src/components/animated-task-label.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, memo } from 'react'
+import { Pressable } from 'react-native'
+import { Text, HStack, Box } from 'native-base'
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSequence,
+  withDelay,
+  interpolateColor
+} from 'react-native-reanimated'
+
+interface Props {
+  strikethrough: boolean
+  textColor: string
+  inactiveTextColor: string
+  onPress?: () => void
+  children?: React.ReactNode
+}
+
+const AnimatedBox = Animated.createAnimatedComponent(Box)
+const AnimatedHStack = Animated.createAnimatedComponent(HStack)
+const AnimatedText = Animated.createAnimatedComponent(Text)
+
+const AnimatedTaskLabel = memo((props: Props) => {
+  const { inactiveTextColor, strikethrough, textColor, children, onPress } =
+    props
+
+  const hStackOffset = useSharedValue(0)
+  const hStackAnimatedStyles = useAnimatedStyle(
+    () => ({
+      transform: [{ translateX: hStackOffset.value }]
+    }),
+    [strikethrough]
+  )
+
+  const textColorProgress = useSharedValue(0)
+  const textColorAnimatedStyles = useAnimatedStyle(
+    () => ({
+      color: interpolateColor(
+        textColorProgress.value,
+        [0, 1],
+        [textColor, inactiveTextColor]
+      )
+    }),
+    [strikethrough, textColor, inactiveTextColor]
+  )
+
+  useEffect(() => {
+    const easing = Easing.out(Easing.quad)
+    if (strikethrough) {
+      hStackOffset.value = withSequence(
+        withTiming(4, { duration: 200, easing }),
+        withTiming(0, { duration: 200, easing })
+      )
+      textColorProgress.value = withDelay(
+        1000,
+        withTiming(1, { duration: 400, easing })
+      )
+    } else {
+      textColorProgress.value = withTiming(0, { duration: 400, easing })
+    }
+  })
+
+  return (
+    <Pressable onPress={onPress}>
+      <AnimatedHStack alignItems="center" style={[hStackAnimatedStyles]}>
+        <AnimatedText
+          fontSize={19}
+          noOfLines={1}
+          isTruncated
+          px={1}
+          style={[textColorAnimatedStyles]}
+        >
+          {children}
+        </AnimatedText>
+        <AnimatedBox position="absolute" h={1} borderBottomWidth={1} />
+      </AnimatedHStack>
+    </Pressable>
+  )
+})
+
+AnimatedTaskLabel.displayName = 'AnimatedTaskLabel'
+
+export default AnimatedTaskLabel
